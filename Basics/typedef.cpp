f3c48bdd1d3fae803ba0103ed83192506c21c943
Devang Mehrotra <devang.mehrotra7@gmail.com>
2021-04-01 20:44:07 +0530

    Typedef

---
+++ Basics/typedef.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+// typedef used to define our own datatype or assign datatype
+typedef int marks;
+int main()
+{
+    marks m1, m2; //gives clarity
+    m1 = 90;
+    m2 = 100;
+}
