6c95d2b3d688d1774cc3c08ebdab3a04ab7c57a4
Devang Mehrotra <devang.mehrotra7@gmail.com>
2021-04-12 23:13:34 +0530

    Pointer Arithmetic

--- Pointers/pointer_arithmetic.cpp
+++ Pointers/pointer_arithmetic.cpp
@@ -8,4 +8,20 @@ int main()
     // 3. p=p+n
     // 4. p=p-n
     // 5. d=q-p or p-q i.e subtraction of 2 pointers (gives the distance between them; can be negative also)
+    int A[5] = {2, 4, 6, 8, 10};
+    int *p = A;
+    cout << *p << endl;
+    p++;
+    cout << *p << endl;
+    p--;
+    cout << *p << endl;
+    p += 3;
+    cout << *p << endl;
+    p -= 2;
+    cout << *p << endl;
+    int *q = &A[4];
+    cout << q - p << endl; //distance
+    int *r = &A[0];
+    cout << r - p << endl; //negative distance
+    return 0;
 }
