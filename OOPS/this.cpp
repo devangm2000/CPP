12f0cfac0ccf1b4db16542218b21a42cc51931bb
Devang Mehrotra <devang.mehrotra7@gmail.com>
2021-05-22 19:27:41 +0530

    This Pointer

---
+++ OOPS/this.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+class Rectangle
+{
+private:
+    int length;
+    int breadth;
+
+public:
+    Rectangle(int length, int breadth)
+    {
+        this->length = length;
+        this->breadth = breadth;
+    }
+    int area()
+    {
+        return length * breadth;
+    }
+    int perimeter()
+    {
+        return 2 * (length + breadth);
+    }
+};
+int main()
+{
+    Rectangle r(10, 5);
+    cout << r.area();
+}
